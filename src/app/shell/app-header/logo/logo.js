0c439bced88a2ee795398b995fb710b9c61387be
kostik.noir <simply.konstantin@gmail.com>
2017-01-24 00:40:36 +0200

    feat(): logo

--- src/app/shell/app-header/logo/logo.js
+++ src/app/shell/app-header/logo/logo.js
@@ -1,20 +1,35 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 import styles from './styles.less';
-import img from './assets/logo-white.png';
+import imgWhite from './assets/logo-white.png';
+import imgColor from './assets/logo-color.png';
 
 const propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  inverted: PropTypes.bool
 };
 
 const defaultProps = {
-  className: ''
+  className: '',
+  inverted: false
 };
 
-export default function Logo({ className: extraClassName = '' }) {
-  const className = classNames(styles.logo, extraClassName);
+export default function Logo({ className: extraClassName, inverted }) {
+  const rootClassName = classNames(styles.root, extraClassName);
+  const whiteLogoImgClassName = classNames({
+    [styles.hiddenImg]: inverted,
+    [styles.visibleImg]: !inverted
+  });
+  const colorLogoImgClassName = classNames({
+    [styles.hiddenImg]: !inverted,
+    [styles.visibleImg]: inverted
+  });
+
   return (
-    <img className={className} src={img} />
+    <span className={rootClassName}>
+      <img className={whiteLogoImgClassName} src={imgWhite} />
+      <img className={colorLogoImgClassName} src={imgColor} />
+    </span>
   );
 }
 
