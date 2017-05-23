ba515f1450df71a660b633010aca45810c5f4154
kostik.noir <simply.konstantin@gmail.com>
2017-05-23 11:44:31 +0300

    chore(): update the configuration in order to use next.bity.com

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 const defaultCfg = require('./default');
 
 module.exports = Object.assign({}, defaultCfg, {
-  HOST: 'https://dev.sbex.ch/',
-  CLIENT_ID: '5zwT7JOqGWqD4gEWqdfEAXa7lelyzZk4aiuF03mM'
+  HOST: 'https://next.bity.com',
+  CLIENT_ID: 'hN1ifyjdhAAW4tw0sKkZr8SSUvw1earBiRcTbakY'
 });
